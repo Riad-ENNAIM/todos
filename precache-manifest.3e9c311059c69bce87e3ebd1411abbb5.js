4ec3e4b6160d6509726aad29b3b127abf8764a69
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2019-11-09 18:41:27 +0100

    Updates

---
+++ precache-manifest.3e9c311059c69bce87e3ebd1411abbb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a9b7ef5f98d0098c4253a69208159e",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "12d1ed384b8fe75a39e3",
+    "url": "/todos/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8ac1290c912c277a289d",
+    "url": "/todos/static/js/2.62c2a6a3.chunk.js"
+  },
+  {
+    "revision": "12d1ed384b8fe75a39e3",
+    "url": "/todos/static/js/main.6a139d12.chunk.js"
+  },
+  {
+    "revision": "58f72a9928444ac95a71",
+    "url": "/todos/static/js/runtime-main.7fb49737.js"
+  }
+]);
